33db1ef813cae964ffac08243c92520dacd69703
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-03-01 10:44:08 +0100

    Tests en cours

--- components/Board.js
+++ components/Board.js
@@ -1,37 +1,46 @@
 import {Image, StyleSheet, Text, ToastAndroid, View} from "react-native";
 import React from "react";
+import GestureRecognizer from "react-native-swipe-gestures";
 
 const Board = (props) => {
+
+    const handleSwipe = (gestureState) => {
+
+        switch (gestureState.direction) {
+            case "SWIPE_LEFT":
+                console.log("Swiped left");
+                break;
+            case "SWIPE_RIGHT":
+                console.log("Swiped right");
+                break;
+            case "SWIPE_UP":
+                console.log("Swiped up");
+                break;
+            case "SWIPE_DOWN":
+                console.log("Swiped down");
+                break;
+        }
+    };
+
     return (
-        <View style={styles.container}
-              onTouchStart={e=> {
-                  this.touchY = e.nativeEvent.pageY;
-                  this.touchX = e.nativeEvent.pageX;
-              }}
-              onTouchEnd={e => {
-                  if (this.touchY - e.nativeEvent.pageY > 20)
-                      ToastAndroid.show('up', ToastAndroid.SHORT);
-                  if (this.touchY - e.nativeEvent.pageY < -20)
-                      ToastAndroid.show('down', ToastAndroid.SHORT);
-                  if (this.touchX - e.nativeEvent.pageX > 20)
-                      ToastAndroid.show('left', ToastAndroid.SHORT);
-                  if (this.touchX - e.nativeEvent.pageX < -20)
-                      ToastAndroid.show('right', ToastAndroid.SHORT);
-              }}>
+        <GestureRecognizer onSwipe={handleSwipe}>
+        <View style={styles.container}>
             {props.board.map((row, rowIndex) => (
                 <View key={rowIndex}>
                     {row.map((cell, cellIndex) => (
-                        <Text key={cellIndex} style={[styles.cell, cell === '#' && styles.wall]}>
+                        <View key={cellIndex} style={[styles.cell, cell === '#' && styles.wall]}>
                             {cell === '.' && <Image source={require("../assets/background.png")}/>}
                             {cell === '#' && <Image source={require("../assets/wall.png")}/>}
                             {cell === 'P' && <Image source={require("../assets/character.png")}/>}
                             {cell === 'B' && <Image source={require("../assets/box.png")}/>}
                             {cell === 'X' && <Image source={require("../assets/cible.png")}/>}
-                        </Text>
+                        </View>
                     ))}
                 </View>
             ))}
         </View>
+        </GestureRecognizer>
+
     );
 }
 
@@ -48,18 +57,6 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         lineHeight: 30,
         fontWeight: 'bold',
-    },
-    wall: {
-        backgroundColor: 'black',
-        color: 'white',
-    },
-    player: {
-        backgroundColor: 'blue',
-        color: 'white',
-    },
-    destination: {
-        backgroundColor: 'green',
-        color: 'white',
     }
 });
 
