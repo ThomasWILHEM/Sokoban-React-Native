afcec99944e6d6a6a46aae7d4e527edd9cf53198
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-03-01 11:16:20 +0100

    Ajout de fonctions utilisées pour les déplacements

--- components/Board.js
+++ components/Board.js
@@ -5,33 +5,53 @@ import Player from "./Player";
 const Board = (props) => {
 
     const [swipeDirection, setSwipeDirection] = useState('');
+    const [board, setBoard] = useState(props.board);
 
     const panResponder = useRef(
         PanResponder.create({
             onStartShouldSetPanResponder: () => true,
-            onPanResponderMove: (evt, gestureState) => {
+            onPanResponderMove: () => setSwipeDirection(''),
+            onPanResponderRelease: (evt, gestureState) => {
                 const { dx, dy } = gestureState;
                 if (Math.abs(dx) > Math.abs(dy)) {
                     setSwipeDirection(dx > 0 ? 'right' : 'left');
+                    if(dx > 0){
+
+                    }
+                    else{
+
+                    }
+
                 } else {
                     setSwipeDirection(dy > 0 ? 'down' : 'up');
                 }
-            },
-            onPanResponderRelease: () => setSwipeDirection(''),
+    }
         })
     ).current;
 
+    const getPlayerPostition = () =>{
+        board.forEach((row,rowIndex) =>{
+            row.forEach((cell,cellIndex) => {
+                if(cell === "P")
+                    return [rowIndex,cellIndex];
+            })
+        });
+    }
+
+    const getObjectToPosition = (rowIndex, cellIndex) => {
+        return board[rowIndex][cellIndex];
+    }
 
     return (
         <View  {...panResponder.panHandlers}>
             <View style={styles.container} >
-                {props.board.map((row, rowIndex) => (
+                {board.map((row, rowIndex) => (
                     <View key={rowIndex}>
                         {row.map((cell, cellIndex) => (
                             <View key={cellIndex} style={[styles.cell, cell === '#' && styles.wall]}>
                                 {cell === '.' && <Image source={require("../assets/background.png")}/>}
                                 {cell === '#' && <Image source={require("../assets/wall.png")}/>}
-                                {cell === 'P' && <Player/> }
+                                {cell === 'P' && <Player cords={[rowIndex,cellIndex]}/>}
                                 {cell === 'B' && <Image source={require("../assets/box.png")}/>}
                                 {cell === 'X' && <Image source={require("../assets/cible.png")}/>}
                             </View>
--- components/Player.js
+++ components/Player.js
@@ -1,24 +1,7 @@
 import React, { useRef, useState } from 'react';
-import {View, Text, StyleSheet, PanResponder, Image} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 const Player = () => {
-    const [swipeDirection, setSwipeDirection] = useState('');
-
-    const panResponder = useRef(
-        PanResponder.create({
-            onStartShouldSetPanResponder: () => true,
-            onPanResponderMove: (evt, gestureState) => {
-                const { dx, dy } = gestureState;
-                if (Math.abs(dx) > Math.abs(dy)) {
-                    setSwipeDirection(dx > 0 ? 'right' : 'left');
-                } else {
-                    setSwipeDirection(dy > 0 ? 'down' : 'up');
-                }
-            },
-            onPanResponderRelease: () => setSwipeDirection(''),
-        })
-    ).current;
-
     return (
         <Image source={require("../assets/character.png")}/>
     );
